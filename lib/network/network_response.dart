7cbc8c0e75fca1a71bd86663806ad69e326bf4c0
Dean <42195191+DeanDever@users.noreply.github.com>
2023-11-19 19:51:39 +0800

    添加 JSON Serializable

--- lib/network/network_response.dart
+++ lib/network/network_response.dart
@@ -1,20 +1,18 @@
-// import 'litchat.g.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'network_response.g.dart';
 
 @JsonSerializable()
 class NetworkResponse {
 
-  @JsonKey(name: 'statue')
+  @JsonKey(name: 'status')
   int status = -1;
   @JsonKey(name: 'content')
   Map? content;
   @JsonKey(name: 'error')
   String? error;
 
-  // factory NetworkResponse.fromJson(Map<String, dynamic> json) => _$NetworkResponseFromJson(json);
-  // Map<String, dynamic> toJson() => _$NetworkResponseToJson(this);
+  NetworkResponse({required this.status, this.content, this.error});
 
-// NetworkResponse.fromJson(Map<String, dynamic> json)
-  //     : status = json['status'],
-  //       content = json['content'],
-  //       error = json['error'];
+  factory NetworkResponse.fromJson(Map<String, dynamic> json) => _$NetworkResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$NetworkResponseToJson(this);
 }
